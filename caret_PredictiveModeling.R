1e3d438d87fd16e9f2ce2486eaf3e9eb0a4132ef
sky0502 <helenyan624@gmail.com>
2018-11-01 14:11:02 -0400

    Update and rename caret.R to caret_PredictiveModeling.R

--- caret_PredictiveModeling.R
+++ caret_PredictiveModeling.R
@@ -1,13 +1,30 @@
+#######################################################
+#      Streamline Predictive Modeling with caret       #
+#           http://topepo.github.io/caret/             #
+########################################################
+
+# caret, short for _C_lassification _A_nd _RE_gression _T_raining
+# Available models in caret: http://topepo.github.io/caret/available-models.html
+# author Max Kuhn, PhD in Biostatistics, Software Engineer at RStudio, former non-clinical Statistician at Pfizer, making order out of chaos
+# Short introduction to caret: https://cran.r-project.org/web/packages/caret/vignettes/caret.html
+# Kuhn, Max. "Caret package." Journal of statistical software 28.5 (2008): 1-26.
+# Book Applied Predictive Modeling http://appliedpredictivemodeling.com/
+
 set.seed(666)
+#### Getting started ####
+# install caret package for the fist time
+install.packages("caret")
+# Load packages
 library(caret)
 library(AppliedPredictiveModeling)
 library(mlbench)
-
+# load dataset
 data(BostonHousing)
+# Explore data
 summary(BostonHousing)
 dim(BostonHousing)
 ggplot(data = BostonHousing, aes(x = medv)) + geom_histogram()
-
+# Visualization
 theme1 <- trellis.par.get()
 theme1$plot.symbol$col = rgb(.2, .2, .2, .4)
 theme1$plot.symbol$pch = 16
@@ -21,6 +38,7 @@ featurePlot(x = BostonHousing[, c("age", "lstat", "tax")],
             span = .5,
             layout = c(3, 1))
 
+#### Pre-processing ####
 # corr
 descrCor <- cor(BostonHousing[,-4])
 findCorrelation(descrCor, cutoff = .8) #tax & rad
@@ -32,6 +50,7 @@ pp <- preProcess(BostonHousing[, -14],  method = c("center", "scale"))
 pp
 transformed <- predict(pp, newdata = BostonHousing[, -14])
 
+#### Splitting the data ####
 # split continuous
 inTrain <- createDataPartition(
   y = BostonHousing$medv,  ## the outcome data are needed
@@ -57,7 +76,11 @@ nrow(training2)
 nrow(testing2)
 
 #### trainning classification ####
-# Logistic regression
+# Penalized Logistic regression
+plrFit <- train(Class ~ ., data = training2, 
+                 method = "plr", 
+                 trControl = fitControl)
+plrFit
 # Nearest Neighbor
 # Random Forest
 # Support Vector Machine
@@ -79,3 +102,4 @@ lassoFit <- train(medv ~ ., data = training,
 lassoFit
 
 #### Variable importance ####
+#### Making predictions ####
